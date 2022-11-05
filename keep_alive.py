0ef9dd1e5c74b932c94128efbab1f77ef41dbe70
Rhys Yim <79439703+rhysyim@users.noreply.github.com>
2022-11-05 16:49:26 +0000

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Hello. I am alive!"
+    return "London Bus Checker Telegram Bot - https://github.com/rhysyim/londonBusCheckerTelegramBot"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
